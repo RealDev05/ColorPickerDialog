3064bc992dc3553cf622a5f301843b7253434104
RealDev05 <binil10032005@gmail.com>
2022-07-01 02:16:49 +0800

    Initial Commit

--- app/src/main/java/com/realdev/test/MainActivity.java
+++ app/src/main/java/com/realdev/test/MainActivity.java
@@ -3,6 +3,9 @@ package com.realdev.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.realdev.colorpickerdialog.ColorPickerDialogBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ColorPickerDialogBuilder colorPickerDialog=new ColorPickerDialogBuilder(this);
+        colorPickerDialog.show(new ColorPickerDialogBuilder.ColorPickedListener() {
+            @Override
+            public void OnColorPicked(int color, String hex) {
+                Toast.makeText(MainActivity.this, "int : "+color+"\n hex : "+hex, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
